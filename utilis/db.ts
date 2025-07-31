cc0174a91b133be21a6225d13a96548202d8d692
Mohamad Wakkaf <107024291+Mohamad16593@users.noreply.github.com>
2025-07-31 20:32:16 +0300

    Update db.ts

--- utilis/db.ts
+++ utilis/db.ts
@@ -3,13 +3,13 @@
 import { MongoClient, Db } from "mongodb";
 const uri = process.env.NUXT_MONGODB_URI!;
 const client = new MongoClient(uri);
-const dbName = process.env.NUXT_MONGODB_DB!;
+
 
 let cachedDb: Db | null = null;
 
 export async function getDb() {
   if (!cachedDb) {
-    await client.connect();
+    await client.connect("chairs_db");
     cachedDb = client.db(dbName);
   }
   return cachedDb;
