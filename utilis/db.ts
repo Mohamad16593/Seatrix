75532991658753d3f828deea6b7b55ae61ad2fa2
Mohamad Wakkaf <107024291+Mohamad16593@users.noreply.github.com>
2025-07-31 22:43:53 +0300

    Update db.ts

--- utilis/db.ts
+++ utilis/db.ts
@@ -1,16 +1,14 @@
-//Connect to the DB
-
 import { MongoClient, Db } from "mongodb";
+
 const uri = process.env.NUXT_MONGODB_URI!;
 const client = new MongoClient(uri);
-
 
 let cachedDb: Db | null = null;
 
 export async function getDb() {
   if (!cachedDb) {
-    await client.connect("chairs_db");
-    cachedDb = client.db(" chairs_db");
+    await client.connect(); // No argument here
+    cachedDb = client.db("chairs_db"); // No leading space
   }
   return cachedDb;
 }
